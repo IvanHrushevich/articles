2cb9f152cd27324eb5a266864a59ce2598cca88c
Ivan Grushevich <ivan.grushevich1@gmail.com>
2025-02-25 00:44:22 +0300

    refactor: implement useTheme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
+import { Suspense } from 'react';
 import { Link, Route, Routes } from 'react-router-dom';
 import { Counter } from './components/Counter';
-import './styles/index.scss';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
-import { Suspense, useContext } from 'react';
-import { Theme, ThemeContext } from './theme/ThemeContext';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
 
 export const App = () => {
-  const { theme, setTheme } = useContext(ThemeContext);
-
-  const toggleTheme = () => {
-    setTheme(theme === Theme.Dark ? Theme.Light : Theme.Dark);
-  };
-
+  const { theme, toggleTheme } = useTheme();
   return (
     <div className={`app ${theme}`}>
       <button onClick={toggleTheme}>Toggle theme</button>
+
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
