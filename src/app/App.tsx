84f24b789b4dc78eeac23e4814c13099405d0ca2
Ivan Grushevich <ivan.grushevich1@gmail.com>
2025-04-01 14:49:07 +0300

    refactor: implement config for router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import { useTheme } from 'app/providers/ThemeProvider';
+import { Link } from 'react-router-dom';
 import { classNames } from 'shared/lib/classNames';
-import { Suspense } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { AppRoter } from './providers/router';
 import './styles/index.scss';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -16,12 +14,7 @@ export const App = () => {
 
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'about'} element={<AboutPage />}></Route>
-          <Route path={'/'} element={<MainPage />}></Route>
-        </Routes>
-      </Suspense>
+      <AppRoter></AppRoter>
     </div>
   );
 };
