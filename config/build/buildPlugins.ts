7edeb6e5f45f99a66b90aa0b7cfaa1ae79166637
Ivan Grushevich <ivan.grushevich1@gmail.com>
2025-04-04 12:44:30 +0300

    feat: add i18n

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,7 +4,10 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 import { BuildOptions } from './types/config';
 
-export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({
+  paths,
+  isDev,
+}: BuildOptions): webpack.WebpackPluginInstance[] {
   return [
     new HtmlWebpackPlugin({
       template: paths.html,
@@ -14,5 +17,6 @@ export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInst
       filename: 'css/[name].[contenthash:8].css',
       chunkFilename: 'css/[name].[contenthash:8].css',
     }),
+    new webpack.DefinePlugin({ __IS_DEV__: isDev }),
   ];
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,38 @@ import { Navbar } from 'widgets/Navbar';
 import { AppRoter } from './providers/router';
 import './styles/index.scss';
 import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
+
+// TODO: remove
+function MyComponent() {
+  const { t, i18n } = useTranslation();
+
+  const toggle = () => {
+    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+  };
+
+  return (
+    <div>
+      <button onClick={toggle}>{t('Перевод')}</button>
+      <h1>{t('Тестовый пример')}</h1>
+    </div>
+  );
+}
 
 export const App = () => {
   const { theme } = useTheme();
 
   return (
-    <div className={classNames('app', {}, [theme])}>
-      <Navbar />
-      <div className="content-page">
-        <Sidebar />
-        <AppRoter />
+    <Suspense fallback="">
+      <div className={classNames('app', {}, [theme])}>
+        <Navbar />
+        <MyComponent />
+        <div className="content-page">
+          <Sidebar />
+          <AppRoter />
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
