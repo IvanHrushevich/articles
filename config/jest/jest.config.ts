25c48e0e0b5e9e4515b05c42f14a6cc5b6245adf
Ivan Grushevich <ivan.grushevich1@gmail.com>
2025-04-06 11:52:10 +0300

    chore: fix jest config (make jest handle absolute paths)

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -7,28 +7,13 @@ import type { Config } from 'jest';
 
 const config: Config = {
     clearMocks: true,
-    testEnvironment: "jsdom",
-    coveragePathIgnorePatterns: [
-        "/node_modules/"
-    ],
-    moduleDirectories: [
-        "node_modules"
-    ],
-
-    moduleFileExtensions: [
-        "js",
-        "mjs",
-        "cjs",
-        "jsx",
-        "ts",
-        "tsx",
-        "json",
-        "node"
-    ],
-    rootDir: "../..",
-    testMatch: [
-        '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'
-    ],
+    testEnvironment: 'jsdom',
+    coveragePathIgnorePatterns: ['/node_modules/'],
+    moduleDirectories: ['node_modules', 'src'],
+    modulePaths: ['<rootdir>src'],
+    moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node'],
+    testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'],
+    rootDir: '../..',
     // All imported modules in your tests should be mocked automatically
     // automock: false,
 
